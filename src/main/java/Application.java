9be29f23337a53e7c2f52e24245c8d61c0738e22
catking562 <taewookim562@gmail.com>
2024-04-04 17:50:27 +0900

    animaldata 명령어 기능 추가

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,9 +1,14 @@
+import taewookim.Animal;
+import taewookim.AnimalTypes;
 import util.World;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 public class Application {
 
@@ -20,16 +25,29 @@ public class Application {
                 isend = true;
                 break;
             case "animalcount":
-                writer.write("동물 수 : " + w.getAnimalsCount() + "/" + w.getAnimalsMax());
+                writer.write("동물 수 : " + w.getAnimalsCount() + "/" + w.getAnimalsMax()+"\n");
                 writer.flush();
                 break;
             case "animaldata":
+                Map<AnimalTypes, Integer> countmap = new HashMap<>();
+                for(AnimalTypes t : AnimalTypes.values()) {
+                    countmap.put(t, 0);
+                }
+                Iterator<Animal> iter = w.getAnimals();
+                while(iter.hasNext()) {
+                    AnimalTypes t = iter.next().getType();
+                    countmap.put(t, countmap.get(t)+1);
+                }
+                for(Map.Entry<AnimalTypes, Integer> entry : countmap.entrySet()) {
+                    writer.write(entry.getKey().toString()+": "+entry.getValue()+"\n");
+                }
+                writer.flush();
                 break;
             case "help":
                 writer.write("stop - 프로그램 정지\n");
                 writer.write("animalcount - 동물 숫자 계산\n");
                 writer.write("animaldata - 동물 데이터 계산\n");
-                writer.write("help - 도움말");
+                writer.write("help - 도움말\n");
                 writer.flush();
                 break;
         }
